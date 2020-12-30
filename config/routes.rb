fd8436a23ca88a991f8545e246b7a317dda873ee
Sugai-Ayano <b3w41067a@gmail.com>
2020-12-30 06:21:33 +0000

    [Fix]Bookers2-debug

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
   resources :books
-
-  root 'homes#top'
   get 'home/about', to: 'homes#about', as: 'home_about'
   root to: 'homes#top'
 end
